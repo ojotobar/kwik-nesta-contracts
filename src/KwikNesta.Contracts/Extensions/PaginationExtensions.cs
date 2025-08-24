f0197225088c40373d2a815ad63d5b2bfd4d9a7e
Toba Ojo <ojotobar@gmail.com>
2025-08-24 23:38:05 +0100

    Implemented pagination

---
+++ src/KwikNesta.Contracts/Extensions/PaginationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using KwikNesta.Contracts.Models;
+
+namespace KwikNesta.Contracts.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static Paginator<T> Paginate<T>(this IEnumerable<T> source,
+                                               int pageNumber,
+                                               int pageSize)
+        {
+            var count = source.Count();
+            var items = source
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return new Paginator<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
---
+++ src/KwikNesta.Contracts/Models/PageQuery.cs
@@ -0,0 +1,22 @@
+﻿namespace KwikNesta.Contracts.Models
+{
+    public class PageQuery
+    {
+        private int _page = 1;
+        private int _pageSize = 10;
+        private int _maxPageSize = 50;
+
+        public int Page
+        {
+            get => _page; 
+            set => _page = value <= 0 ? _page : value;
+        }
+
+        public int PageSize 
+        { 
+            get => _pageSize; 
+            set => _pageSize = value <= 0 ? _pageSize : 
+                value > _maxPageSize ? _maxPageSize : value; 
+        }
+    }
+}
