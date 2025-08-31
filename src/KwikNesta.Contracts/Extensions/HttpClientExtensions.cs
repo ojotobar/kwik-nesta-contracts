191dae344c679f44c870d4767a76d1b383015ef1
Toba Ojo <ojotobar@gmail.com>
2025-08-31 14:27:36 +0100

    Added http client deserializer extension method

---
+++ src/KwikNesta.Contracts/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace KwikNesta.Contracts.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T?> ReadFromJsonAsync<T>(this HttpResponseMessage response,
+                                                          JsonSerializerOptions? options = null)
+        {
+            response.EnsureSuccessStatusCode();
+            var stream = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<T>(stream, options ?? new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+    }
+}
