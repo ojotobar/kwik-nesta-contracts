275ed24f08a5425c777a9f792a58844ac3e1f5c2
Toba Ojo <ojotobar@gmail.com>
2025-08-31 13:51:25 +0100

    Added location related models

---
+++ src/KwikNesta.Contracts/Models/Location/KwikNestaTimeZone.cs
@@ -0,0 +1,11 @@
+﻿namespace KwikNesta.Contracts.Models.Location
+{
+    public class KwikNestaTimeZone
+    {
+        public string ZoneName { get; set; } = string.Empty;
+        public long GMTOffset { get; set; }
+        public string GMTOffsetName { get; set; } = string.Empty;
+        public string Abbreviation { get; set; } = string.Empty;
+        public string TZName { get; set; } = string.Empty;
+    }
+}
