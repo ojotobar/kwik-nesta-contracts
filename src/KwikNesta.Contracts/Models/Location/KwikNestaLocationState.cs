6872aeeaea2eab05d4e96e180885a5f73a359e1d
Toba Ojo <ojotobar@gmail.com>
2025-09-28 04:58:55 +0100

    Added location models

---
+++ src/KwikNesta.Contracts/Models/Location/KwikNestaLocationState.cs
@@ -0,0 +1,20 @@
+﻿using EFCore.CrudKit.Library.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace KwikNesta.Contracts.Models.Location
+{
+    public class KwikNestaLocationState : EntityBase
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string CountryCode { get; set; } = string.Empty;
+        public string ISO2 { get; set; } = string.Empty;
+        public string Longitude { get; set; } = string.Empty;
+        public string Latitude { get; set; } = string.Empty;
+        public string? Type { get; set; }
+
+        public Guid CountryId { get; set; }
+        public KwikNestaLocationState? Country { get; set; }
+        public List<KwikNestaLocationCity> Cities { get; set; } = [];
+    }
+}
---
+++ src/KwikNesta.Contracts/Models/Location/KwikNestaLocationTimeZone.cs
@@ -0,0 +1,20 @@
+﻿using EFCore.CrudKit.Library.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace KwikNesta.Contracts.Models.Location
+{
+    public class KwikNestaLocationTimeZone : EntityBase
+    {
+        public Guid CountryId { get; set; }
+        public KwikNestaLocationCountry? Country { get; set; }
+        [Required]
+        public string ZoneName { get; set; } = string.Empty;
+        [Required]
+        public long GMTOffset { get; set; }
+
+        public string GMTOffsetName { get; set; } = string.Empty;
+        public string Abbreviation { get; set; } = string.Empty;
+        [Required]
+        public string TZName { get; set; } = string.Empty;
+    }
+}
