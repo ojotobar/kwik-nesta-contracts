31f4df6378d0127e64f2a26719cbfd2c26d345f9
Toba Ojo <ojotobar@gmail.com>
2025-09-28 05:20:39 +0100

    Added audit log model

---
+++ src/KwikNesta.Contracts/Models/KwikNestaAuditLog.cs
@@ -0,0 +1,21 @@
+﻿using EFCore.CrudKit.Library.Models;
+using KwikNesta.Contracts.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace KwikNesta.Contracts.Models
+{
+    public class KwikNestaAuditLog : EntityBase
+    {
+        [Required]
+        public AuditDomain Domain { get; set; }
+        [Required]
+        public Guid DomainId { get; set; }
+        [Required]
+        public AuditAction Action { get; set; }
+        [Required]
+        public string PerformedBy { get; set; } = string.Empty;
+        [Required]
+        public string PerformedOnProfileId { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
