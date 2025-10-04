98cf094ef783455a1c9a51c4c42b743a07663793
Toba Ojo <ojotobar@gmail.com>
2025-10-04 22:34:14 +0100

    Added enums

---
+++ src/KwikNesta.Contracts/Enums/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace KwikNesta.Contracts.Enums
+{
+    public enum Gender
+    {
+        [Description("Others")]
+        Others,
+        [Description("Male")]
+        Male,
+        [Description("Female")]
+        Female
+    }
+}
---
+++ src/KwikNesta.Contracts/Enums/SuspensionReasons.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace KwikNesta.Contracts.Enums
+{
+    public enum SuspensionReasons
+    {
+        [Description("Account suspended due to unusual or suspicious login activity.")]
+        SuspiciousActivity,
+        [Description("Account suspended because it may have been compromised.")]
+        CompromisedAccount,
+        [Description("Account suspended for violating terms of service or community guidelines.")]
+        PolicyViolation,
+        [Description("Account suspended for fraudulent or malicious behavior.")]
+        FraudulentBehavior,
+        [Description("Account suspended due to repeated failed payments.")]
+        PaymentFailure,
+        [Description("Account temporarily suspended pending investigation.")]
+        PendingInvestigation,
+        [Description("Account suspended due to duplicate or multiple accounts violation.")]
+        DuplicateAccount,
+        [Description("Account suspended after multiple user reports.")]
+        UserReports,
+        [Description("Account suspended for abusing system resources or spamming.")]
+        SystemAbuse,
+        [Description("Account suspended for other administrative reasons.")]
+        Other
+    }
+}
