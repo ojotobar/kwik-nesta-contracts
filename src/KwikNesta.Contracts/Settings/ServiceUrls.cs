edbcaf09b179812a82df9cc78bc96231d7b99287
Toba Ojo <ojotobar@gmail.com>
2025-10-14 23:52:06 +0100

    Added settings classes and hangfire auth filter

---
+++ src/KwikNesta.Contracts/Settings/ServiceUrls.cs
@@ -0,0 +1,11 @@
+﻿namespace KwikNesta.Infrastruture.Svc.API.Settings
+{
+    public class ServiceUrls
+    {
+        public string GatewayService { get; set; } = string.Empty;
+        public string IdentityService { get; set; } = string.Empty;
+        public string SupportService { get; set; } = string.Empty;
+        public string PropertyService { get; set; } = string.Empty;
+        public string PaymentService { get; set; } = string.Empty;
+    }
+}
