3fcdc4e453e6453738bb1fa567f397936c0166c5
Toba Ojo <ojotobar@gmail.com>
2025-10-18 15:55:17 +0100

    Removed reference to unused packaged Added PagedData model

---
+++ src/KwikNesta.Contracts/Models/PagedData.cs
@@ -0,0 +1,18 @@
+﻿namespace KwikNesta.Contracts.Models
+{
+    public class PagedData<T>
+    {
+        public MetaData? MetaData { get; set; }
+        public List<T> Data { get; set; } = [];
+    }
+
+    public class MetaData
+    {
+        public int Page { get; set; }
+        public int Size { get; set; }
+        public int Pages { get; set; }
+        public bool HasNext { get; set; }
+        public bool HasPrevious { get; set; }
+        public int Count { get; set; }
+    }
+}
