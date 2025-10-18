818a19b2dffcb80d27ec8f24d229c3a303fbe08d
Toba Ojo <ojotobar@gmail.com>
2025-10-18 16:26:06 +0100

    Added base entity to the contract package

---
+++ src/KwikNesta.Contracts/Models/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿namespace KwikNesta.Contracts.Models
+{
+    public abstract class EntityBase
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public bool IsDeprecated { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public DateTime LastUpdatedOn { get; set; } = DateTime.UtcNow;
+    }
+}
